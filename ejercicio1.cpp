62236dd5dde150e016be64b5bae9f304218a2f93
MarielMu <132585225+MarielMu@users.noreply.github.com>
2023-05-22 11:47:22 -0600

    Add files via upload

---
+++ ejercicio1.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+using namespace std;
+
+
+int main() {
+	string n;
+	int respuesta;
+	printf ("ingrese un numero: " )endl;
+	scanf ( n;
+	if (n=="11") {
+		respuesta = 0;
+		printf( "corecto"  )endl;
+	} else {
+		printf ( "incorrecto" )endl;
+	}
+	return 0;
+}
+
